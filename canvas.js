417c9e140b4c71fe911749476358f2d0764c268e
Lukas Singer <spelled.with.k@gmail.com>
2021-05-30 22:59:18 -0500

    Basic layout

--- canvas.js
+++ canvas.js
@@ -1,7 +1,14 @@
-const canvas = document.getElementById('canvas').getContext('2d');
+const config = import("./config.json");
+const canvasElement = document.getElementById('canvas')
+const canvas = canvasElement.getContext('2d');
 
 canvas.fillStyle = 'rgb(200, 0, 0)';
 canvas.fillRect(10, 10, 50, 50);
 
 canvas.fillStyle = 'rgba(0, 0, 200, 0.5)';
 canvas.fillRect(30, 30, 50, 50);
+
+window.onresize = function () {
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight - config.toolbarHeight;
+}
