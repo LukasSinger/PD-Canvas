20ec0396d9ab2ec1d1dcad22194794d75556a9a9
Lukas Singer <spelled.with.k@gmail.com>
2021-05-30 19:08:21 -0500

    Import files

---
+++ canvas.js
@@ -0,0 +1,9 @@
+import p5 from './p5.min.js';
+const canvas = document.getElementById('canvas').getContext('2d');
+throw 'It works'
+
+canvas.fillStyle = 'rgb(200, 0, 0)';
+canvas.fillRect(10, 10, 50, 50);
+
+canvas.fillStyle = 'rgba(0, 0, 200, 0.5)';
+canvas.fillRect(30, 30, 50, 50);
