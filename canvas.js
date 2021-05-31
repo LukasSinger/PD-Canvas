94b051c4afe3334f5527aa4592bc8451761b10c5
Lukas Singer <spelled.with.k@gmail.com>
2021-05-31 11:01:36 -0500

    Add responsive design

--- canvas.js
+++ canvas.js
@@ -1,14 +1,32 @@
-const config = import("./config.json");
-const canvasElement = document.getElementById('canvas')
+const bottomToolbar = document.getElementById('bottomToolbar');
+const canvasElement = document.getElementById('canvas');
 const canvas = canvasElement.getContext('2d');
+init();
 
-canvas.fillStyle = 'rgb(200, 0, 0)';
-canvas.fillRect(10, 10, 50, 50);
+async function init() {
+    await fetch("./config.json")
+    .then(response => response.json())
+    .then(data => {
+        resizeCanvas()
+        drawCanvas()
+        setInterval(function () {
+            if (canvasElement.width != window.innerWidth || canvasElement.height != window.innerHeight - parseInt(window.getComputedStyle(bottomToolbar).getPropertyValue('height'))) {
+                resizeCanvas()
+                drawCanvas()
+            }
+        }, 300);
+    });
+};
 
-canvas.fillStyle = 'rgba(0, 0, 200, 0.5)';
-canvas.fillRect(30, 30, 50, 50);
+function drawCanvas() {
+    canvas.fillStyle = 'rgb(200, 0, 0)';
+    canvas.fillRect(10, 10, 50, 50);
 
-window.onresize = function () {
+    canvas.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    canvas.fillRect(30, 30, 50, 50);
+};
+
+function resizeCanvas() {
     canvasElement.width = window.innerWidth;
-    canvasElement.height = window.innerHeight - config.toolbarHeight;
-}
+    canvasElement.height = window.innerHeight - parseInt(window.getComputedStyle(bottomToolbar).getPropertyValue('height'));
+};
