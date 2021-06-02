de7d8229014fa1da9e6596109f3510c071165afb
Lukas Singer <spelled.with.k@gmail.com>
2021-06-01 19:22:22 -0500

    Fix fallback (add proper catch)

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -17,27 +17,25 @@ async function init() {
 function main(config) {
     setup();
     saveButton.onclick = function() {
-        try {
-            let date = new Date();
-            let canvasExport;
-            fetch(document.getElementById('defaultCanvas0').toDataURL())
-            .then(res => res.blob())
-            .then(blob => {
-                return new File(
-                    [blob],
-                    `Sketch from ${new Intl.DateTimeFormat('default', { hour: 'numeric', minute: 'numeric' }).format(date)} on ${new Intl.DateTimeFormat('default', { year: 'numeric', month: 'long', day: 'numeric' }).format(date)}.png`,
-                    {type: blob.type}
-                );
-            })
-            .then(file => {
-                navigator.share({
-                    'files': [ file ],
-                    mimeType: 'image/png'
-                });
+        let date = new Date();
+        let canvasExport;
+        fetch(document.getElementById('defaultCanvas0').toDataURL())
+        .then(res => res.blob())
+        .then(blob => {
+            return new File(
+                [blob],
+                `Sketch from ${new Intl.DateTimeFormat('default', { hour: 'numeric', minute: 'numeric' }).format(date)} on ${new Intl.DateTimeFormat('default', { year: 'numeric', month: 'long', day: 'numeric' }).format(date)}.png`,
+                {type: blob.type}
+            );
+        })
+        .then(file => {
+            navigator.share({
+                'files': [ file ],
+                mimeType: 'image/png'
             });
-        } catch(e) {
-            saveCanvas();
-        }
+        })
+        // If share fails for some reason, regular download prompt will appear
+        .catch(err => saveCanvas());
     };
 };
 
